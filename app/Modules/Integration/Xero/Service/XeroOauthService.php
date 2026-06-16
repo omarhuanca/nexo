904e8c47c5a072be0eae892a2f4ae12674b1cbea
Wally Kevin Zeballos Oquendo <kevin-wal@hotmail.com>
2026-06-16 09:36:53 -0400

    Configuracion

--- app/Modules/Integration/Xero/Service/XeroOauthService.php
+++ app/Modules/Integration/Xero/Service/XeroOauthService.php
@@ -21,7 +21,7 @@ class XeroOauthService
         $this->provider = new GenericProvider([
             'clientId' => config('xero.client_id'),
             'clientSecret' => config('xero.client_secret'),
-            'redirectUri' => 'https://backendnexo.shop/api/integrations/xero/callback',
+            'redirectUri' => config('xero.redirect_uri'),
             'urlAuthorize' => config('xero.url_authorize'),
             'urlAccessToken' => config('xero.url_access_token'),
             'urlResourceOwnerDetails' => config('xero.url_resource_owner'),
@@ -83,7 +83,7 @@ class XeroOauthService
                 'grant_type' => 'authorization_code',
                 'client_id' => config('xero.client_id'),
                 'client_secret' => config('xero.client_secret'),
-                'redirect_uri' => 'https://backendnexo.shop/api/integrations/xero/callback',
+                'redirect_uri' => config('xero.redirect_uri'),
                 'code' => $request->input('code'),
             ]
 
--- config/xero.php
+++ config/xero.php
@@ -3,7 +3,7 @@
 return [
     'client_id' => env('XERO_CLIENT_ID'),
     'client_secret' => env('XERO_CLIENT_SECRET'),
-    'redirect_uri'=> env('https://backendnexo.shop/api/integrations/xero/callback'),
+    'redirect_uri'=> env('XERO_REDIRECT_URI'),
     'scopes' => env(
         'XERO_SCOPES',
         'openid email profile offline_access accounting.settings accounting.transactions accounting.contacts'
